4953418281728c8d2c3449a49e60df8603860a7a
Stephen Lee <stephenleejm@gmail.com>
2014-04-02 14:30:53 +0800

    Removed comment from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='CherryPy-Test',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='A sample CherryPy app',
+      author='Stephen Lee',
+      author_email='',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3','CherryPy>=3.2.5'],
+      install_requires=['Django>=1.3','CherryPy>=3.2.5'],
      )
